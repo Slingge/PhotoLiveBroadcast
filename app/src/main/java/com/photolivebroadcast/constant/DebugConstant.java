9618e54df710b001328d60d4c0c2eefd44b401c8
Slingge <493724235@qq.com>
2018-10-08 20:21:33 +0800

    //

---
+++ app/src/main/java/com/photolivebroadcast/constant/DebugConstant.java
@@ -0,0 +1,5 @@
+package com.photolivebroadcast.constant;
+
+public class DebugConstant {
+    public final static String DEBUG_TAG="xingyun";
+}
---
+++ app/src/main/java/com/photolivebroadcast/constant/LoginConstant.java
@@ -0,0 +1,6 @@
+package com.photolivebroadcast.constant;
+
+public class LoginConstant {
+    public final static String LOGIN_TAG_FILE_NAME="login_tag_file_name";
+    public final static String LOGIN_TAG_FILE_KEY="login_tag_file_key";
+}
